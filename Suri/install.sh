820033286a77af6969e77a0044aab46ac86789b1
Radu Dita <hades2510@yahoo.com>
2024-03-28 12:01:26 +0200

    fix HEREDOCS for Suri instal strategy

--- Suri/install.sh
+++ Suri/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $SSH_CONNECTION_STRING -i $SSH_KEY "DOWNLOAD_URL='$DOWNLOAD_URL'" 'bash -s' <<'EOF'
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $SSH_CONNECTION_STRING -i $SSH_KEY "DOWNLOAD_URL='$DOWNLOAD_URL'" 'bash -s' <<EOF
 curl -o suri.zip -L $DOWNLOAD_URL
 unzip suri.zip -d app
 cd app/$(ls app)
