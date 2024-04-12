000c73fdab47608cf721c1b6056f88a61263cd1e
Radu Dita <hades2510@yahoo.com>
2024-04-12 14:48:43 +0300

    take into account config file for Suri links

--- Suri/install.sh
+++ Suri/install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+# copy config file if provided
+if [-z "${JSON_CONFIG_FILE}"]; then
+    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $SSH_CONNECTION_STRING -i $SSH_KEY $JSON_CONFIG_FILE "$SSH_CONNECTION_STRING:~/config.json"
+fi
+
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $SSH_CONNECTION_STRING -i $SSH_KEY "DOWNLOAD_URL='$DOWNLOAD_URL'" 'bash -s' <<'EOF'
 curl -o suri.zip -L $DOWNLOAD_URL
 unzip suri.zip -d app
 cd app/$(ls app)
+[ -f "~/config.json" ] && cp ~/config.json ./src/links.json
 npm install
 nohup npm run dev -- --port 8080 > /dev/null 2>&1 &
 EOF
