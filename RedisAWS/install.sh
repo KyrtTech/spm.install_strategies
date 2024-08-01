5b428c40f5a3cee376735a32b31e68ffbda55e2c
Radu Dita <hades2510@yahoo.com>
2024-08-01 17:36:35 +0300

    write output to provided SPM file

--- RedisAWS/install.sh
+++ RedisAWS/install.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 npx cdk bootstrap
-npx cdk deploy --require-approval never
+npx cdk deploy --require-approval never --outputs-file output.json
+
+if [ -z "${SMP_OUTPUT_PATH}" ]; then
+    echo "No output for SMP"
+else
+    jq  '{
+        output_params: {
+            redisPort: (.RedisAwsStack.RedisPort | tonumber),
+            redisUsername: .RedisAwsStack.RedisUsernameOutput,
+            redisHost: .RedisAwsStack.RedisHost,
+            redisPassword: .RedisAwsStack.RedisPasswordOutput
+        }
+    }' output.json > $SMP_OUTPUT_PATH
+fi
