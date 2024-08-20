1778d9590f9c5a403c63f0be5a41c4d579907778
Radu Dita <hades2510@yahoo.com>
2024-08-20 22:16:28 +0300

    add helper script for updating .env file

--- Kutt/install.sh
+++ Kutt/install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o SendEnv="SPM_*" $SSH_CONNECTION_STRING -i $SSH_KEY "DOWNLOAD_URL='$DOWNLOAD_URL'" 'bash -s' <<'EOF'
+# copy update env config file
+if [ -n "update_env.sh" ]; then
+    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $SSH_KEY "update_env.sh" "$SSH_CONNECTION_STRING":~/update_env.sh
+fi
+
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o SendEnv="REDIS_PORT REDIS_USERNAME REDIS_HOST REDIS_PASSWORD DATABASE_URL DATABASE_NAME DATABASE_USERNAME DATABASE_PASSWORD DATABASE_PORT" $SSH_CONNECTION_STRING -i $SSH_KEY "DOWNLOAD_URL='$DOWNLOAD_URL'" 'bash -s' <<'EOF'
 curl -o kutt.zip -L $DOWNLOAD_URL
 unzip kutt.zip -d app
 cd app/$(ls app)
-[ -f ~/config.json ] && cp ~/config.json ./src/links.json
 npm install
 cp .example.env .env
+cp ~/update_env.sh .
+chmod +x ./update_env.sh
+./update_env.sh
 npm run build
 nohup npm run start -- --port 8080 > /dev/null 2>&1 &
 EOF
@@ -15,7 +22,6 @@ if [ -z "${SPM_OUTPUT_PATH}" ]; then
     echo "No output for SPM"
 else
     output_params="{ \"output_params\": {
-        \"url\": \"http://${IP}:8080\"
     }}"
 
     echo "Saving outputs to ${SPM_OUTPUT_PATH}"
---
+++ Kutt/update_env.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+CONFIG_FILE=".env"
+
+declare -A env_vars=(
+    ["DATABASE_PORT"]="DB_PORT"
+    ["DATABASE_HOST"]="DB_HOST"
+    ["DATABASE_PASSWORD"]="DB_PASSWORD"
+    ["DATABASE_NAME"]="DB_NAME"
+    ["DATABASE_USERNAME"]="DB_USER"
+    ["REDIS_HOST"]="REDIS_HOST"
+    ["REDIS_PORT"]="REDIS_PORT"
+    ["REDIS_PASSWORD"]="REDIS_PASSWORD"
+    ["PORT"]="PORT"
+)
+
+export PORT=8080
+
+# Function to update the config file
+update_config() {
+    local key=$1
+    local value=$2
+
+    # Use sed to find and replace the key with the new value in the config file
+    sed -i.bak "s|^$key=.*|$key=$value|" "$CONFIG_FILE"
+}
+
+for key in "${!env_vars[@]}"; do
+    update_config "${env_vars[$key]}" "$key"
+done
